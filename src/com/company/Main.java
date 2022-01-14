77fc72f3212bf98b25014e4d942c9f673fdae387
Jakoby Reyez <es7723be@go.minnstate.edu>
2022-01-14 13:02:21 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	System.out.println("Hello World!");
+    System.out.println("JreyezEx1a");
+    }
+    //Oh okay this is how I do comments in Java, this is way better then <!-- -->
+}
